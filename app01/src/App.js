a20ae531fabbace358695a64dc54b4a86f0fc4dd
André Oliveira <andrelitoscria@gmail.com>
2025-01-07 20:25:55 -0300

    ;

---
+++ app01/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <section>
+        <p>André Luis</p> <br/>
+        <p>Rapper e jogador de basquete</p>
+      </section>
+    </>
+  );
+}
+
